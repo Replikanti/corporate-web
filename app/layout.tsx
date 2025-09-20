fafaaf4c10bde6b77ab91917588941c60c740a59
Martin Holy <mholy1983@protonmail.com>
2025-09-20 12:10:38 +0200

    fix: import type Viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Navigation } from '@/components/navigation';
 import { Footer } from '@/components/footer';
-import { Viewport } from 'next';
+import type { Viewport } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
